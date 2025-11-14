aed521ed3b6935ec93fc756e2f7be9aa016ace4d
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-11-14 14:10:19 +0000

    Update limits_modal.php

--- admin/includes/limits_modal.php
+++ admin/includes/limits_modal.php
@@ -1,49 +1,46 @@
-<!-- includes/limits_modal.php -->
+<!-- Edit Limits Modal -->
 <div class="modal fade" id="editLimits">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-        <h4 class="modal-title"><b>Edit Deposit & Withdrawal Limits</b></h4>
-      </div>
-      <div class="modal-body">
-        <form class="form-horizontal" id="limitsForm">
-          <div class="form-group">
-            <label for="edit_min_deposit" class="col-sm-3 control-label">Min Deposit</label>
-            <div class="col-sm-9">
-              <input type="number" class="form-control" id="edit_min_deposit" name="min_deposit" required>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                <h4 class="modal-title"><b>Edit Deposit and Withdrawal Limits</b></h4>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="edit_max_deposit" class="col-sm-3 control-label">Max Deposit</label>
-            <div class="col-sm-9">
-              <input type="number" class="form-control" id="edit_max_deposit" name="max_deposit" required>
+            <div class="modal-body">
+                <form class="form-horizontal" method="POST" action="limits_edit.php">
+                    <input type="hidden" class="limitid" name="id" value="1">
+                    <div class="form-group">
+                        <label for="edit_min_deposit" class="col-sm-3 control-label">Minimum Deposit</label>
+                        <div class="col-sm-9">
+                            <input type="number" step="0.01" class="form-control" id="edit_min_deposit" name="min_deposit" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit_max_deposit" class="col-sm-3 control-label">Maximum Deposit</label>
+                        <div class="col-sm-9">
+                            <input type="number" step="0.01" class="form-control" id="edit_max_deposit" name="max_deposit" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit_min_withdraw" class="col-sm-3 control-label">Minimum Withdrawal</label>
+                        <div class="col-sm-9">
+                            <input type="number" step="0.01" class="form-control" id="edit_min_withdraw" name="min_withdraw" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit_max_withdraw" class="col-sm-3 control-label">Maximum Withdrawal</label>
+                        <div class="col-sm-9">
+                            <input type="number" step="0.01" class="form-control" id="edit_max_withdraw" name="max_withdraw" required>
+                        </div>
+                    </div>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="edit_min_withdraw" class="col-sm-3 control-label">Min Withdraw</label>
-            <div class="col-sm-9">
-              <input type="number" class="form-control" id="edit_min_withdraw" name="min_withdraw" required>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
+                </form>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="edit_max_withdraw" class="col-sm-3 control-label">Max Withdraw</label>
-            <div class="col-sm-9">
-              <input type="number" class="form-control" id="edit_max_withdraw" name="max_withdraw" required>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal">
-          <i class="fa fa-close"></i> Close
-        </button>
-        <button type="submit" class="btn btn-primary btn-flat" id="saveLimits">
-          <i class="fa fa-save"></i> Save
-        </button>
-      </div>
+        </div>
     </div>
-  </div>
 </div>
