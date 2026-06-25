5e7cb06d5604eb583b14cc427ed7bdfe2c5b86ed
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-25 11:10:10 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -22,12 +22,44 @@
         </div>
         <div class="col-md-6">
           <ul class="social-link-list d-flex flex-wrap justify-content-md-end justify-content-center">
-            <li><a href="#0" data-toggle="tooltip" data-placement="top" title="Whatsapp"><i class="lab la-whatsapp"></i></a></li>
-            <li><a href="https://t.me/+Gi7t1eMielhjNWFk" data-toggle="tooltip" data-placement="top" title="Telegram"><i class="lab la-telegram"></i></a></li>
-            <li><a href="mailto:support@nexusinsights.it.com" data-toggle="tooltip" data-placement="top" title="Email"><i class="las la-envelope"></i></a></li>
+            <li><a href="wa.me/+447438783028" data-toggle="tooltip" data-placement="top" title="Whatsapp"><i class="lab la-whatsapp"></i></a></li>
+            <li><a href="" data-toggle="tooltip" data-placement="top" title="Telegram"><i class="lab la-telegram"></i></a></li>
+            <li><a href="mailto:cs.nexus.insights@gmail.com" data-toggle="tooltip" data-placement="top" title="Email"><i class="las la-envelope"></i></a></li>
           </ul>
         </div>
       </div>
     </div>
   </div>
 </footer>
+
+<!-- Floating Customer Support Button -->
+<a href="cs-message.php" class="support-float" title="Customer Support">
+    <i class="las la-headset"></i>
+</a>
+
+<style>
+.support-float{
+    position: fixed;
+    bottom: 25px;
+    right: 25px;
+    width: 60px;
+    height: 60px;
+    background: #28a745;
+    color: #fff;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 28px;
+    text-decoration: none;
+    box-shadow: 0 4px 12px rgba(0,0,0,.3);
+    z-index: 9999;
+    transition: all .3s ease;
+}
+
+.support-float:hover{
+    background: #218838;
+    color: #fff;
+    transform: scale(1.1);
+}
+</style>
